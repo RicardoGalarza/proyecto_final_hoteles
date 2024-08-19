b8392bdc7c32b4cc45cf9e7f146f1f26aa4081af
Ricardo Galarza <ricardogalarza1723@gmail.com>
2024-08-19 18:28:50 -0400

    se ignora la carpeta de uploads

--- hotel-back/src/main/java/com/example/demo/controller/HabitacionController.java
+++ hotel-back/src/main/java/com/example/demo/controller/HabitacionController.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -47,7 +48,7 @@ public class HabitacionController {
     }
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE )
-    //@CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin(origins = "http://localhost:3000")
     public ResponseEntity<Habitacion> createHabitacion(
             @RequestParam("nombre") String nombre,
             @RequestParam("descripcion") String descripcion,
