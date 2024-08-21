b05888c2fea9f3a40438a566e523dc470e7a28e6
Ricardo Galarza <ricardogalarza1723@gmail.com>
2024-08-21 14:33:37 -0400

    deje funcionando el back de cuenta controller del cliente

--- hotel-back/src/main/java/com/example/demo/controller/CuentaController.java
+++ hotel-back/src/main/java/com/example/demo/controller/CuentaController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,6 +36,7 @@ public class CuentaController {
     }
 
     @PostMapping
+    @CrossOrigin(origins = "http://localhost:3000")
     public Cuenta createCuenta(@RequestBody Cuenta cuenta) {
         return cuentaService.saveCuenta(cuenta);
     }
